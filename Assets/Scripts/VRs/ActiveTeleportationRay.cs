8830dddccc92cd0a9825fdf41629c213e0e64124
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2022-10-17 12:56:30 +0200

    Part 4 Teleportation update

---
+++ Assets/Scripts/VRs/ActiveTeleportationRay.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ActiveTeleportationRay : MonoBehaviour
+{
+    public GameObject LeftTeleportation;
+    public GameObject RightTeleportation;
+
+    public InputActionProperty LeftActive;
+    public InputActionProperty RightActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LeftTeleportation.SetActive(LeftActive.action.ReadValue<float>() > .1f);
+        RightTeleportation.SetActive(RightActive.action.ReadValue<float>() > .1f);
+    }
+}
